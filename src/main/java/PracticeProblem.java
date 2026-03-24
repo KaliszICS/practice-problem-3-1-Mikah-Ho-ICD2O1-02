d132a1c923cc428fd77e3e7e87568611c4890e0a
Mikah Ho <mikah.ho@student.tdsb.on.ca>
2026-03-24 11:21:04 -0400

    w

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,47 @@
+/**
+ * File: If Statements
+ * Author: Mikah Ho
+ * Date Created: Mar 24, 2026
+ * Date Last Modified: Mar 24, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		
 	}
 
+	//q1
+	public static String evenOrOdd(int num) {
+	    
+	    if (num % 2 != 0) {
+	        return "Odd";
+	    }
+	    
+	    return "Even";
+	}
+	
+	//q2
+	public static String teacherOrStudent(String name) {
+	    
+	    if (name.equals("Kalisz")) {
+	        return "Teacher";
+	    }
+	    
+	    return "Student";
+	}
+
+	//q3
+	public static int fartherFromZero(int num) {
+	    
+	    if (num > 0) {
+	        return num + 5;
+	    }
+	    
+	    if (num == 0) {
+	        return num;
+	    }
+	    
+	    return num - 5;
+	}
 }
